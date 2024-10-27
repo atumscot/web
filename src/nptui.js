40c9b2b0141afb8c314b72ef8b6de2e6d9abcbb7
mvl22 <martin@lucas-smith.co.uk>
2024-10-27 19:45:25 +0000

    Geocoder: Set result to zoom to specified place in a bbox-sensitive way
    Previously for instance, 'Edinburgh' would zoom closely to the centre; this change sets it to the bbox of Edinburgh

--- src/nptui.js
+++ src/nptui.js
@@ -603,11 +603,7 @@ const nptUi = (function () {
 				minLength: 3,
 				debounceSearch: 400,
 				showResultMarkers: false,
-				marker: false,
-				flyTo: {
-					// #!# Ideally should be bounds: ... but this requires using .on and then result, which means bigger changes
-					zoom: 13
-				}
+				marker: false
 			};
 			
 			// Implement the data retrieval and assembly; see: https://maplibre.org/maplibre-gl-geocoder/types/MaplibreGeocoderApi.html
@@ -625,14 +621,14 @@ const nptUi = (function () {
 						const response = await fetch(request);
 						const geojson = await response.json();
 						for (let feature of geojson.features) {
-							// See: https://web.archive.org/web/20210224184722/https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+							// See: https://maplibre.org/maplibre-gl-geocoder/types/CarmenGeojsonFeature.html and https://web.archive.org/web/20210224184722/https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
 							let point = {
 								type: 'Feature',
 								place_name: feature.properties.display_name,
 								properties: feature.properties,
 								text: feature.properties.display_name,
 								place_type: ['place'],
-								center: feature.geometry.coordinates
+								bbox: feature.bbox
 							};
 							features.push (point);
 						}
