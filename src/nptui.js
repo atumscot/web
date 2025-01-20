4dae75fea5e84e783747c311544ad8ad4c405fee
mvl22 <martin@lucas-smith.co.uk>
2025-01-20 22:57:03 +0000

    Load build file
    This must be load as a blocking file, as otherwise the date may not be available at the point the welcome screen is shown

--- src/nptui.js
+++ src/nptui.js
@@ -50,6 +50,7 @@ const nptUi = (function () {
 	
 	// Settings
 	let _settings = {};		// Will be populated by constructor
+	let _build = {};		// Will be populated by constructor
 	let _datasets = {};		// Will be populated by constructor
 	
 	// Properties
@@ -66,9 +67,26 @@ const nptUi = (function () {
 		// Main function
 		initialise: function (settings, datasets)
 		{
+			// Load the build data then run the constructor
+			fetch ('/src/build.json')
+				.then (function (response) {return response.json ();})
+				.then (function (data) {
+					_build = data;
+					nptUi.construct (settings, datasets);
+				})
+				.catch (function (error) {
+					console.error ('Error loading build data - could not run application:', error);
+				});
+		},
+		
+		
+		// Constructor
+		construct: function (settings, datasets)
+		{
 			// Populate the settings and datasets class properties
 			_settings = settings;
 			_datasets = datasets;
+			// _build will have been loaded
 			
 			// Parse URL hash state
 			nptUi.parseUrl ();
@@ -132,7 +150,7 @@ const nptUi = (function () {
 			
 			// Set OSM and update dates in the text, if present
 			if (document.getElementById ('osmupdatedate')) {
-				document.getElementById ('osmupdatedate').innerHTML = _settings.osmDate;
+				document.getElementById ('osmupdatedate').innerHTML = nptUi.formatAsUKDate (_build.osmDate);
 			}
 			if (document.getElementById ('updatedate')) {
 				document.getElementById ('updatedate').innerText = nptUi.formatAsUKDate (document.lastModified);
